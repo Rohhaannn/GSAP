21f1496f86a32a8d21d1a1d151e52065cab8c891
Rohan Shingade <shingaderohan96@gmail.com>
2026-06-23 23:09:36 +0530

    project setup done with tailwind, gsap pkgs

---
+++ gsap-project-1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-5xl text-green-500 hover:underline'>
+        <h1>Tailwind is working?  YES 😄 </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
