2d91e007c85757870d58f8b4314d2dd321f04191
Rohan Shingade <shingaderohan96@gmail.com>
2026-07-10 12:22:04 +0530

    Hero, Cocktails, About code reformatted

--- gsap-cocktails/src/components/Hero.jsx
+++ gsap-cocktails/src/components/Hero.jsx
@@ -1,57 +1,61 @@
-import { useGSAP } from "@gsap/react"
-import { SplitText } from 'gsap/all';
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { SplitText } from "gsap/all";
 import { useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 
-
 const Hero = () => {
-
   const videoRef = useRef();
 
-  const isMobile = useMediaQuery({maxWidth: 767})
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
   useGSAP(() => {
-    const heroSplit = new SplitText('.title', {type:"chars, words",});
-    const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
+    const heroSplit = new SplitText(".title", {
+      type: "chars, words",
+    });
 
+    const paragraphSplit = new SplitText(".subtitle", {
+      type: "lines",
+    });
+
+    // Apply text-gradient class once before animating
     heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
       yPercent: 100,
       duration: 1.8,
-      ease: 'expo.out',
-      stagger: 0.06
+      ease: "expo.out",
+      stagger: 0.06,
     });
 
     gsap.from(paragraphSplit.lines, {
       opacity: 0,
       yPercent: 100,
       duration: 1.8,
-      ease: 'expo.out',
+      ease: "expo.out",
       stagger: 0.06,
       delay: 1,
-    })
+    });
 
-    gsap.timeline({
-      scrollTrigger: {
-        trigger: '#hero',
-        start: 'top top',
-        end: 'bottom top',
-        scrub: true,
-      }
-    })
-    .to(".right-leaf", { y: 300 }, 0)
-    .to(".left-leaf", { y: -500 }, 0)
-    .to(".arrow", { y: 100 }, 0);
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
 
-    const startValue = isMobile ? 'top 50%' : 'center 60%';
-    const endValue = isMobile ? '120% top' : 'bottom top';
-
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
 
     let tl = gsap.timeline({
       scrollTrigger: {
-        trigger: 'video',
+        trigger: "video",
         start: startValue,
         end: endValue,
         scrub: true,
@@ -64,14 +68,12 @@ const Hero = () => {
         currentTime: videoRef.current.duration,
       });
     };
-
   }, []);
-
 
   return (
     <>
       <section id="hero" className="noisy">
-        <h1 className="title"> MOJITO </h1>
+        <h1 className="title">MOJITO</h1>
 
         <img
           src="/images/hero-left-leaf.png"
@@ -85,23 +87,24 @@ const Hero = () => {
         />
 
         <div className="body">
-          <img src="/images/arrow.png" alt="arrow" className="arrow" />
+          {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
 
           <div className="content">
             <div className="space-y-5 hidden md:block">
-              <p> Cool. Crisp. Classic </p>
+              <p>Cool. Crisp. Classic.</p>
               <p className="subtitle">
-                Sip the spirit <br /> of summer
+                Sip the Spirit <br /> of Summer
               </p>
             </div>
 
             <div className="view-cocktails">
               <p className="subtitle">
-                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed to delight your senses.
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes — designed to delight your
+                senses.
               </p>
-              <a href="#cocktails"> View Cocktails </a>
+              <a href="#cocktails">View cocktails</a>
             </div>
-
           </div>
         </div>
       </section>
@@ -116,8 +119,7 @@ const Hero = () => {
         />
       </div>
     </>
+  );
+};
 
-  )
-}
-
-export default Hero
+export default Hero;
